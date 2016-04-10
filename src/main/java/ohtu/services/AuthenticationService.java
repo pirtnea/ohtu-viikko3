fdf838d1010fc8b8acc26fc77791dc8ec4b47014
Nea <pine@cs.helsinki.fi>
2016-04-10 19:27:00 +0300

    Viikon 3 repo

---
+++ src/main/java/ohtu/services/AuthenticationService.java
@@ -0,0 +1,54 @@
+package ohtu.services;
+
+import ohtu.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import ohtu.data_access.UserDao;
+
+public class AuthenticationService {
+
+    private UserDao userDao;
+
+    public AuthenticationService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public boolean logIn(String username, String password) {
+        for (User user : userDao.listAll()) {
+            if (user.getUsername().equals(username)
+                    && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createUser(String username, String password) {
+        if (userDao.findByName(username) != null) {
+            return false;
+        }
+
+        if (invalid(username, password)) {
+            return false;
+        }
+
+        userDao.add(new User(username, password));
+
+        return true;
+    }
+
+    private boolean invalid(String username, String password) {
+        // validity check of username and password
+	return usernameCheck(username) || passwordCheck(password);
+    }
+
+    private boolean usernameCheck(String username) {
+	return (username.length() < 3 || !username.matches("[a-zA-Z]+"));
+    }
+
+    private boolean passwordCheck(String password) {
+	return (password.length() < 8 || password.matches("^[a-zA-Z]+$"));
+    }
+}
+
